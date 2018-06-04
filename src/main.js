f447a638d2df3fb1971f3dc5f802fab6dea5ca98
Charlie Frye <32540979+LeMentore@users.noreply.github.com>
2018-06-04 15:50:40 +0500

    added routes and components

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App'
+import { routes } from './routes'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
 new Vue({
     el: '#app',
+    router,
     components: {App},
     template: '<App/>'
 })
