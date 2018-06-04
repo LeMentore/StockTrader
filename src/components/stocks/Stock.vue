1592092a83025a7e87c62517f546f8ada593e3e2
Charlie Frye <32540979+LeMentore@users.noreply.github.com>
2018-06-04 16:46:41 +0500

    added stocks components

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,5 +1,22 @@
 <template>
-
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    ИМЯ
+                    <small>(Цена: ЦЕНА)</small>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <input type="number" class="form-control" placeholder="Количество">
+                </div>
+                <div class="pull-right">
+                    <button class="btn btn-success">Купить</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
