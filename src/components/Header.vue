c42178c3420a3398a4d3778054d3f7d737e6801d
Charlie Frye <32540979+LeMentore@users.noreply.github.com>
2018-06-04 16:21:47 +0500

    added menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,32 @@
 <template>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+            </div>
 
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <router-link to="/portfolio" activeClass="active" tag="li"><a>Портфель</a></router-link>
+                    <router-link to="/stocks" activeClass="active" tag="li"><a>Акции</a></router-link>
+                </ul>
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Конец дня</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            Сохранить/Загрузить
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Сохранить данные</a></li>
+                            <li><a href="#">Загрузить данные</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
